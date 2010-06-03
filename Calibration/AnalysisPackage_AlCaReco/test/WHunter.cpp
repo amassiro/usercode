07fc0b00b512ababd80aeb2b8a43cc63eec1ab94
amassiro <>
2010-06-03 14:47:27 +0000

    new selections

--- Calibration/AnalysisPackage_AlCaReco/test/WHunter.cpp
+++ Calibration/AnalysisPackage_AlCaReco/test/WHunter.cpp
@@ -46,10 +46,14 @@ int main(int argc, char** argv)
  int entryMIN = gConfigParser -> readIntOption("Input::entryMIN");
  int entryMOD = gConfigParser -> readIntOption("Input::entryMOD");
  
+ int BinMass = gConfigParser -> readIntOption("Input::BinMass");
+ 
  
  std::cout << ">>>>> input::entryMIN  " << entryMIN  << std::endl;  
  std::cout << ">>>>> input::entryMAX  " << entryMAX  << std::endl;  
  std::cout << ">>>>> input::entryMOD  " << entryMOD  << std::endl;  
+ 
+ std::cout << ">>>>> input::BinMass   " << BinMass  << std::endl;  
  
  // Open ntple
  TChain* chain = new TChain(treeName.c_str());
@@ -68,7 +72,7 @@ int main(int argc, char** argv)
  outFile.cd();
  
  TH1F hPt("hPt","hPt",2000,0,2000);
- TH1F hMT("hMT","hMT",20,0,200);
+ TH1F hMT("hMT","hMT",BinMass,0,200);
  
  
  double start, end;
@@ -85,29 +89,92 @@ int main(int argc, char** argv)
   
   int nEles = reader.Get4V("electrons")->size();
   int nEleSel = 0;
-  std::cerr << " nEles = " << nEles << " " ;
   
   std::vector<int> blacklist;
   for (int iEle = 0; iEle < nEles; iEle++){    
-   if (((reader.GetFloat("electrons_tkIso")->at(iEle)    > 1.966
-    || reader.GetFloat("electrons_emIso03")->at(iEle)    > 3.155
-    || reader.GetFloat("electrons_hadIso03_1")->at(iEle) > 8.739
-    || reader.GetFloat("electrons_hOverE")->at(iEle)     > 0.033
-    ) && (fabs((reader.Get4V("electrons")->at(iEle)).Eta()) < 1.479))
-    ||
-    ((reader.GetFloat("electrons_tkIso")->at(iEle)       > 2.136
-    || reader.GetFloat("electrons_emIso03")->at(iEle)    > 7.991
-    || reader.GetFloat("electrons_hadIso03_1")->at(iEle) > 1.762
-    || reader.GetFloat("electrons_hOverE")->at(iEle)     > 0.034
-    ) && (fabs((reader.Get4V("electrons")->at(iEle)).Eta()) > 1.479))
-    )
+   if
+//     (
+//     reader.GetInt("electrons_mishits")->at(iEle) > 1
+//     || reader.GetFloat("electrons_deltaPhiIn")->at(iEle) > 0.1
+//     || reader.GetFloat("electrons_deltaEtaIn")->at(iEle) > 0.02
+//     || reader.Get4V("electrons")->at(iEle).Pt() < 5
+//     || (reader.GetFloat("electrons_eOverP")->at(iEle) > 5 || reader.GetFloat("electrons_eOverP")->at(iEle) < 0.35)
+//     || reader.GetFloat("electrons_sigmaIetaIeta")->at(iEle) < 0.02
+//     || (reader.Get4V("met")->at(0)).P() < 30
+//     || reader.GetFloat("electrons_hOverE")->at(iEle) > 0.2
+//     || reader.GetFloat("electrons_tkIso")->at(iEle) > 2.0
+//     || reader.GetFloat("electrons_emIso04")->at(iEle) > 3.0
+//     || reader.GetFloat("electrons_hadIso04_1")->at(iEle) > 2.0    
+//     || reader.GetFloat("electrons_hadIso03_1")->at(iEle) > 2.0
+//     )
+
+
+// (
+// ((reader.GetInt("electrons_mishits")->at(iEle) > 1
+// || reader.GetFloat("electrons_tkIso")->at(iEle)      > 1.966
+// || reader.GetFloat("electrons_emIso03")->at(iEle)    > 3.155
+// || reader.GetFloat("electrons_hadIso03_1")->at(iEle) > 8.739
+// || reader.GetFloat("electrons_hOverE")->at(iEle)     > 0.033
+// || reader.GetFloat("electrons_deltaPhiIn")->at(iEle) > 0.032
+// || reader.GetFloat("electrons_deltaEtaIn")->at(iEle) > 0.019
+// //     || reader.GetFloat("electrons_sigmaIetaIeta")->at(iEle) > 0.010
+// ) && (fabs((reader.Get4V("electrons")->at(iEle)).Eta()) < 1.479))
+// ||
+// ((reader.GetInt("electrons_mishits")->at(iEle) > 1
+// || reader.GetFloat("electrons_tkIso")->at(iEle)      > 1.136
+// || reader.GetFloat("electrons_emIso03")->at(iEle)    > 7.991
+// || reader.GetFloat("electrons_hadIso03_1")->at(iEle) > 1.762
+// || reader.GetFloat("electrons_hOverE")->at(iEle)     > 0.034
+// || reader.GetFloat("electrons_deltaPhiIn")->at(iEle) > 0.025
+// || reader.GetFloat("electrons_deltaEtaIn")->at(iEle) > 0.008
+// //     || reader.GetFloat("electrons_sigmaIetaIeta")->at(iEle) > 0.031
+// ) && (fabs((reader.Get4V("electrons")->at(iEle)).Eta()) > 1.479))
+
+(
+ (reader.Get4V("met")->at(0)).Et() < 20
+ || reader.Get4V("electrons")->at(iEle).Pt() < 20 || reader.Get4V("electrons")->at(iEle).Pt() > 60
+ || deltaPhi(reader.Get4V("electrons")->at(iEle).Phi(),(reader.Get4V("met")->at(0)).Phi()) < 0.75
+ || reader.GetFloat("sumET")->at(0) > 40
+ || (reader.GetFloat("sumET")->at(0) > 10 && reader.Get4V("met")->at(0).Et() < (30 + 2/15. * (reader.GetFloat("sumET")->at(0)-25)))
+ || (reader.GetFloat("sumET")->at(0) < 10 && reader.Get4V("met")->at(0).Et() < (20 + 0.8 * (reader.GetFloat("sumET")->at(0))))
+)
+
+///=== old ===
+//    (
+//      ((reader.GetInt("electrons_mishits")->at(iEle) > 1
+//     || reader.GetFloat("electrons_tkIso")->at(iEle)      > 1.966
+//     || reader.GetFloat("electrons_emIso03")->at(iEle)    > 3.155
+//     || reader.GetFloat("electrons_hadIso03_1")->at(iEle) > 8.739
+//     || reader.GetFloat("electrons_hOverE")->at(iEle)     > 0.033
+//     || reader.GetFloat("electrons_deltaPhiIn")->at(iEle) > 0.032
+//     || reader.GetFloat("electrons_deltaEtaIn")->at(iEle) > 0.019
+// //     || reader.GetFloat("electrons_sigmaIetaIeta")->at(iEle) > 0.010
+//     ) && (fabs((reader.Get4V("electrons")->at(iEle)).Eta()) < 1.479))
+//     ||
+//      ((reader.GetInt("electrons_mishits")->at(iEle) > 1
+//     || reader.GetFloat("electrons_tkIso")->at(iEle)      > 1.136
+//     || reader.GetFloat("electrons_emIso03")->at(iEle)    > 7.991
+//     || reader.GetFloat("electrons_hadIso03_1")->at(iEle) > 1.762
+//     || reader.GetFloat("electrons_hOverE")->at(iEle)     > 0.034
+//     || reader.GetFloat("electrons_deltaPhiIn")->at(iEle) > 0.025
+//     || reader.GetFloat("electrons_deltaEtaIn")->at(iEle) > 0.008
+// //     || reader.GetFloat("electrons_sigmaIetaIeta")->at(iEle) > 0.031
+//     ) && (fabs((reader.Get4V("electrons")->at(iEle)).Eta()) > 1.479))
+//     || (reader.Get4V("met")->at(0)).P() < 30
+//     || reader.Get4V("electrons")->at(iEle).Pt() < 20
+//    )
+    {
     blacklist.push_back(iEle);
+   }
+    
   }
   
   
   int iEleMaxPt  = SelectLepton(*reader.Get4V("electrons"),"maxPt",5,&blacklist);
-  std::cerr << " iEleMaxPt = " << iEleMaxPt << std::endl;
-
+  if (iEleMaxPt!= -1) {
+   std::cerr << " nEles = " << nEles << " " ;
+   std::cerr << " iEleMaxPt = " << iEleMaxPt << std::endl;
+  }
   for (int iEle = 0; iEle < nEles; iEle++){    
    bool skipEle = false;
    
@@ -115,12 +182,13 @@ int main(int argc, char** argv)
     if(blacklist.at(kk) == static_cast<int>(iEle)) skipEle = true;
    }
    if (iEle == iEleMaxPt) skipEle = true;
-   if (reader.GetFloat("Calo_Energy")->at(iEle) < 5.0) skipEle = true;
+   if (reader.GetFloat("Calo_Energy")->at(iEle) < 10.0) skipEle = true;
    if (!skipEle) {
     nEleSel++;
    }
   }
-  if (iEleMaxPt != -1 && nEleSel == 0){
+//   if (iEleMaxPt != -1){
+   if (iEleMaxPt != -1 && nEleSel == 0){
    hPt.Fill(reader.Get4V("electrons")->at(iEleMaxPt).Pt());
    
    double MT = mT(reader.Get4V("met")->at(0),(reader.Get4V("electrons")->at(iEleMaxPt)));
