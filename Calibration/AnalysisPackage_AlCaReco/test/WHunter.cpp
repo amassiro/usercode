1a17a3152854534522dfb2ce6a16f7995b922003
amassiro <>
2010-06-07 14:36:43 +0000

    updates

--- Calibration/AnalysisPackage_AlCaReco/test/WHunter.cpp
+++ Calibration/AnalysisPackage_AlCaReco/test/WHunter.cpp
@@ -47,6 +47,7 @@ int main(int argc, char** argv)
  int entryMOD = gConfigParser -> readIntOption("Input::entryMOD");
  
  int BinMass = gConfigParser -> readIntOption("Input::BinMass");
+ int BinPt   = gConfigParser -> readIntOption("Input::BinPt");
  
  
  std::cout << ">>>>> input::entryMIN  " << entryMIN  << std::endl;  
@@ -54,6 +55,16 @@ int main(int argc, char** argv)
  std::cout << ">>>>> input::entryMOD  " << entryMOD  << std::endl;  
  
  std::cout << ">>>>> input::BinMass   " << BinMass  << std::endl;  
+ std::cout << ">>>>> input::BinPt     " << BinPt  << std::endl;  
+ 
+ double PTMIN = gConfigParser -> readDoubleOption("Selections::PTMIN");
+ std::cout << ">>>>> Selections::PTMIN  " << PTMIN  << std::endl;  
+ 
+ double MINMT = gConfigParser -> readDoubleOption("Selections::MINMT");
+ std::cout << ">>>>> Selections::MINMT  " << MINMT  << std::endl;  
+ 
+ 
+ 
  
  // Open ntple
  TChain* chain = new TChain(treeName.c_str());
@@ -71,12 +82,31 @@ int main(int argc, char** argv)
  std::string outFileNameImage    = gConfigParser -> readStringOption("Output::outFileNameImage");
  std::cout << ">>>>> Output::outFileNameImage  " << outFileNameImage  << std::endl;  
  
+ std::string outFileNameImagePT    = gConfigParser -> readStringOption("Output::outFileNameImagePT");
+ std::cout << ">>>>> Output::outFileNameImagePT  " << outFileNameImagePT  << std::endl;  
+ 
+ std::string outFileNameImageET    = gConfigParser -> readStringOption("Output::outFileNameImageET");
+ std::cout << ">>>>> Output::outFileNameImageET  " << outFileNameImageET  << std::endl;  
+ 
  
  TFile outFile(OutFileName.c_str(),"RECREATE");
  outFile.cd();
  
- TH1F hPt("hPt","hPt",2000,0,2000);
+ TH1F hEt("hEt","hEt",BinPt,0,200);
+ TH1F hPt("hPt","hPt",BinPt,0,200);
  TH1F hMT("hMT","hMT",BinMass,0,200);
+ TTree myTree("myTree","myTree");
+ double Eta;
+ double pT;
+ double ET;
+ double MT;
+ double EoP;
+ 
+ myTree.Branch("Eta",&Eta,"Eta/D");
+ myTree.Branch("ET",&ET,"ET/D");
+ myTree.Branch("pT",&pT,"pT/D");
+ myTree.Branch("MT",&MT,"MT/D");
+ myTree.Branch("EoP",&EoP,"EoP/D");
  
  
  double start, end;
@@ -221,41 +251,59 @@ if
     //barrel = cms.vdouble(0.05, 0.0103, 0.8, 0.00688, -1, -1, 7.33, 4.68, 9999., 9999., 9999.,9999., 0.000, -9999., 9999., 9999., 9999, -1, 0, 0),
     //endcap = cms.vdouble(0.0389, 0.0307, 0.7, 0.00944, -1, -1, 7.76, 3.09, 2.23, 9999., 9999.,9999., 0.000, -9999., 9999., 9999., 9999, -1, 0, 0)
     
-    (
-    (
-    ( reader.GetFloat("electrons_hOverE")->at(iEle)            < 0.05
-    && reader.GetFloat("electrons_sigmaIetaIeta")->at(iEle)    < 0.0103
-    && fabs(reader.GetFloat("electrons_deltaPhiIn")->at(iEle)) < 0.8
-    && fabs(reader.GetFloat("electrons_deltaEtaIn")->at(iEle)) < 0.00688
-    && reader.GetFloat("electrons_tkIso")->at(iEle)            < 7.33
-    && reader.GetFloat("electrons_emIso03")->at(iEle)          < 4.68
-    )
-    && (fabs((reader.Get4V("electrons")->at(iEle)).Eta()) < 1.479))
-    ||
-    (
-    ( reader.GetFloat("electrons_hOverE")->at(iEle)            < 0.0389
-    && reader.GetFloat("electrons_sigmaIetaIeta")->at(iEle)    < 0.0307
-    && fabs(reader.GetFloat("electrons_deltaPhiIn")->at(iEle)) < 0.7
-    && fabs(reader.GetFloat("electrons_deltaEtaIn")->at(iEle)) < 0.00944
-    && reader.GetFloat("electrons_tkIso")->at(iEle)            < 7.76
-    && reader.GetFloat("electrons_emIso03")->at(iEle)          < 3.09
-    )
-    && (fabs((reader.Get4V("electrons")->at(iEle)).Eta()) >= 1.479))
-    )
-    &&
+//     (
+//     (
+//     ( reader.GetFloat("electrons_hOverE")->at(iEle)            < 0.05
+//     && reader.GetFloat("electrons_sigmaIetaIeta")->at(iEle)    < 0.0103
+//     && fabs(reader.GetFloat("electrons_deltaPhiIn")->at(iEle)) < 0.8
+//     && fabs(reader.GetFloat("electrons_deltaEtaIn")->at(iEle)) < 0.00688
+//     && reader.GetFloat("electrons_tkIso")->at(iEle)            < 7.33
+//     && reader.GetFloat("electrons_emIso03")->at(iEle)          < 4.68
+//     )
+//     && (fabs((reader.Get4V("electrons")->at(iEle)).Eta()) < 1.479))
+//     ||
+//     (
+//     ( reader.GetFloat("electrons_hOverE")->at(iEle)            < 0.0389
+//     && reader.GetFloat("electrons_sigmaIetaIeta")->at(iEle)    < 0.0307
+//     && fabs(reader.GetFloat("electrons_deltaPhiIn")->at(iEle)) < 0.7
+//     && fabs(reader.GetFloat("electrons_deltaEtaIn")->at(iEle)) < 0.00944
+//     && reader.GetFloat("electrons_tkIso")->at(iEle)            < 7.76
+//     && reader.GetFloat("electrons_emIso03")->at(iEle)          < 3.09
+//     )
+//     && (fabs((reader.Get4V("electrons")->at(iEle)).Eta()) >= 1.479))
+//     )
+//     &&
      
      ///==== Branson ====
      
-    (reader.Get4V("met")->at(0)).Et() > 20
-    && reader.Get4V("electrons")->at(iEle).Pt() > 20 && reader.Get4V("electrons")->at(iEle).Pt() < 60
+    (
+    reader.Get4V("met")->at(0)).Et() > 20
+    && reader.Get4V("electrons")->at(iEle).Pt() > PTMIN 
+    && reader.Get4V("electrons")->at(iEle).Pt() < 60
     && deltaPhi(reader.Get4V("electrons")->at(iEle).Phi(),(reader.Get4V("met")->at(0)).Phi()) > 0.75
-     //   || reader.GetFloat("sumET")->at(0) < 40
-     //   || (reader.GetFloat("sumET")->at(0) > 10 && reader.Get4V("met")->at(0).Et() > (30 + 2/15. * (reader.GetFloat("sumET")->at(0)-25)))
-     //   || (reader.GetFloat("sumET")->at(0) < 10 && reader.Get4V("met")->at(0).Et() > (20 + 0.8 * (reader.GetFloat("sumET")->at(0))))
+    && (((reader.Get4V("met")->at(0)).Et() / reader.GetFloat("sumEt")->at(0)) > (-0.07 * (reader.Get4V("met")->at(0)).Et() + 3.5 ) 
+    || ((reader.Get4V("met")->at(0)).Et() / reader.GetFloat("sumEt")->at(0)) > 0.4)
+//     && (reader.Get4V("met")->at(0)).Et() > (-0.08 * (reader.Get4V("met")->at(0)).Et() / reader.GetFloat("sumEt")->at(0) +4)
+    
+//      && reader.GetFloat("sumEt")->at(0) < 40
+//      && ((reader.GetFloat("sumEt")->at(0) >= 10 && reader.Get4V("met")->at(0).Et() > (30 + 2/15. * (reader.GetFloat("sumEt")->at(0)-25)))
+//        || (reader.GetFloat("sumEt")->at(0) < 10 && reader.Get4V("met")->at(0).Et() > (20 + 0.8 * (reader.GetFloat("sumEt")->at(0)))))
+    
+ 
+ 
+ ///=== + eleID
+//     reader.Get4V("met")->at(0)).Et() > 18
+//     && reader.Get4V("electrons")->at(iEle).Et() > 18
+//     && reader.Get4V("electrons")->at(iEle).Pt() > PTMIN 
+//     && reader.Get4V("electrons")->at(iEle).Pt() < 60
+//     && deltaPhi(reader.Get4V("electrons")->at(iEle).Phi(),(reader.Get4V("met")->at(0)).Phi()) > 0.75
+//     && (((reader.Get4V("met")->at(0)).Et() / reader.GetFloat("sumEt")->at(0)) > (-0.1 * (reader.Get4V("met")->at(0)).Et() + 2.6 ) 
+//     || ((reader.Get4V("met")->at(0)).Et() / reader.GetFloat("sumEt")->at(0)) > 0.5)
     
     )
      {
       whitelist.push_back(iEle);
+      std::cerr << " reader.GetFloat(\"sumEt\")->at(0) = " << reader.GetFloat("sumEt")->at(0) << std::endl;
      }
      else {
       blacklist.push_back(iEle);
@@ -282,26 +330,52 @@ if
    }
   }
 //   if (iEleMaxPt != -1){
-   if (iEleMaxPt != -1 && nEleSel == 0){
-   hPt.Fill(reader.Get4V("electrons")->at(iEleMaxPt).Pt());
-   
-   double MT = mT(reader.Get4V("met")->at(0),(reader.Get4V("electrons")->at(iEleMaxPt)));
+   if (iEleMaxPt != -1 && nEleSel == 0){   
+   MT = mT(reader.Get4V("met")->at(0),(reader.Get4V("electrons")->at(iEleMaxPt)));
+   pT = reader.Get4V("electrons")->at(iEleMaxPt).Pt();   
+   ET = (reader.GetFloat("electrons_scE")->at(iEleMaxPt)) * sin(reader.Get4V("electrons")->at(iEleMaxPt).Theta());
+   EoP = reader.GetFloat("electrons_eOverP")->at(iEleMaxPt);
+   Eta = reader.Get4V("electrons")->at(iEleMaxPt).Eta();
+   if (MT > MINMT){
+    hPt.Fill(pT);
+    hEt.Fill(ET);
+   }
    hMT.Fill(MT);
+   myTree.Fill();
    selEvents++;
-  }
+   }
  } //loop over the events 
  
  end = clock();
  std::cout <<"Time = " <<  ((double) (end - start)) << " (a.u.)" << std::endl;  
  std::cout << " selEvents = " << selEvents << " : " << entryMAX - entryMIN << std::endl;
+ 
+ hEt.SetMarkerColor(kGreen);
+ hEt.SetMarkerStyle(20);
+ hEt.SetMarkerSize(1.0);
+ hEt.SetLineColor(kGreen);
+ hEt.SetLineWidth(2.0);
+ hEt.Write();
+ hEt.Draw("E");
+ gPad->SaveAs(outFileNameImageET.c_str());
+ hPt.SetMarkerColor(kBlue);
+ hPt.SetMarkerStyle(20);
+ hPt.SetMarkerSize(1.0);
+ hPt.SetLineColor(kBlue);
+ hPt.SetLineWidth(2.0);
  hPt.Write();
- hMT.Write();
+ hPt.Draw("E");
+ gPad->SaveAs(outFileNameImagePT.c_str());
  hMT.SetMarkerColor(kRed);
- hMT.SetLineColor(kRed);
- hMT.SetLineWidth(1.0);
+ hMT.SetMarkerStyle(20);
  hMT.SetMarkerSize(1.0);
+ hMT.SetLineColor(kRed);
+ hMT.SetLineWidth(2.0);
+ hMT.Write();
  hMT.Draw("E");
  gPad->SaveAs(outFileNameImage.c_str());
+ 
+ myTree.Write();
  outFile.Write();
   
  return 0;
