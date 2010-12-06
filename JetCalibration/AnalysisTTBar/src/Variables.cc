4549b07ea23b0fe79d105d7ce998ac40339786a3
amassiro <>
2010-12-06 17:44:33 +0000

    update

--- JetCalibration/AnalysisTTBar/src/Variables.cc
+++ JetCalibration/AnalysisTTBar/src/Variables.cc
@@ -14,6 +14,8 @@ void InitializeTree(Variables& vars, const std::string& outputRootFileName)
   
   vars.m_efficiencyTree -> Branch("XSection", &vars.XSection, "XSection/D");
   vars.m_efficiencyTree -> Branch("dataFlag",     &vars.dataFlag,         "dataFlag/I");
+  vars.m_efficiencyTree -> Branch("numEntriesBefore",     &vars.numEntriesBefore,         "numEntriesBefore/I");
+  
   
   
   
