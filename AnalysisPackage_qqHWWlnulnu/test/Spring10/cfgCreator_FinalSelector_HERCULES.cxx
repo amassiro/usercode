6a99e90f4b71781f8c680a3cb9f9d679248bc5ce
amassiro <>
2011-01-11 15:17:10 +0000

    update

--- AnalysisPackage_qqHWWlnulnu/test/Spring10/cfgCreator_FinalSelector_HERCULES.cxx
+++ AnalysisPackage_qqHWWlnulnu/test/Spring10/cfgCreator_FinalSelector_HERCULES.cxx
@@ -2,34 +2,31 @@
  ///==== cfg creator ====
  
  #include <iomanip>
- 
- char *nameSample[1000];
- char *nameSamplePrefix[1000];
- char *nameSampleTree[1000];
- 
- char* xsection[1000];
+ #include <string>
+
+ char *nameSample[10000];
+ char *nameSampleFile[10000];
+ char* xsection[10000];
  
  int numberOfSamples = 0;
- std::ifstream inFile("test/Spring10/samples.txt");
+ std::ifstream inFile("test/Spring10/samples_skimmed.txt");
  std::string buffer;
  
  while(!inFile.eof()){
   getline(inFile,buffer);
   std::cout << "buffer = " << buffer << std::endl;
   if (buffer != ""){ ///---> save from empty line at the end!
-   if (buffer.at(0) != '#'){
+   if (buffer[0] != '#'){
     std::stringstream line( buffer );       
-    nameSample[numberOfSamples] = new char [1000];
+    nameSample[numberOfSamples] = new char [10000];
     line >> nameSample[numberOfSamples]; 
     std::cout << nameSample[numberOfSamples] << " ";
     
-    nameSamplePrefix[numberOfSamples] = new char [1000];
-    line >> nameSamplePrefix[numberOfSamples];
-    
-    nameSampleTree[numberOfSamples] = new char [1000];
-    line >> nameSampleTree[numberOfSamples];
-    
-    xsection[numberOfSamples] = new char [1000];
+    nameSampleFile[numberOfSamples] = new char [10000];
+    line >> nameSampleFile[numberOfSamples]; 
+    std::cout << nameSampleFile[numberOfSamples] << " ";
+
+    xsection[numberOfSamples] = new char [10000];
     line >> xsection[numberOfSamples]; 
     std::cout << xsection[numberOfSamples] << " ";
     std::cout << std::endl;
@@ -37,65 +34,34 @@
    } 
   }
  }
- 
- 
+
+
+
+ system("mkdir test/Spring10/dir_cfg_skimmed");
  for (int iSample = 0; iSample < numberOfSamples; iSample++){
   std::ofstream myfile;
   char nameFile[1000];
-  sprintf(nameFile,"test/Spring10/dir_cfg/FinalSelection_%s.cfg",nameSample[iSample]);
+  sprintf(nameFile,"test/Spring10/dir_cfg_skimmed/NtupleProducer_%s.cfg",nameSample[iSample]);
   myfile.open (nameFile);
   
   myfile << std::fixed;
   myfile << std::setprecision(20);
   myfile << "[Input]" << std::endl;
-  myfile << "treeName = " << nameSampleTree[iSample] << std::endl;
-  myfile << "inputFile = /gwterax1/users/amassiro/VBF/Spring10/" << nameSamplePrefix[iSample] << "_" << nameSample[iSample] << ".root" << std::endl;
+  myfile << "treeName = SimpleNtple " << std::endl;
+  myfile << "inputFile = /gwterax2/users/govoni/qqH_WW_lvlv/presel/" << nameSampleFile[iSample] << std::endl;
   char toDo[1000];
   sprintf(toDo,"double xsec = %s",xsection[iSample]);
   gROOT->ProcessLine(toDo);
   myfile << "inputXSection = " << xsec << std::endl;
-  myfile << "treeNameLeptonFilter = VBFPtMinLeptonCountFilterAll" << std::endl;
-  myfile << "treeNameJetCounterFilter = VBFEtMinCaloJetCountFilterL2L3Antikt5CaloJets " << std::endl;
+  myfile << "histoNameEvents = events" << std::endl;
   myfile << std::endl;
   myfile << "entryMIN = 0 " << std::endl;
   myfile << "entryMAX = -1 " << std::endl;
   myfile << "entryMOD = 1000 " << std::endl;
+  myfile << "nStepToDo = 4 " << std::endl;
   myfile << std::endl;
   myfile << "[Output]" << std::endl;
-  myfile << "OutFileNameEfficiencies = /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/output_Spring10/out_FinalSelection_" << nameSample[iSample] << ".root" << std::endl;
-  myfile << std::endl;
-  
-  myfile << "[Jet]" << std::endl;
-  myfile << "AdditionalInputFiles = \\" << std::endl;
-  myfile << "   /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/weights_Jet/TMVAnalysis_MLP.weights.xml, \\ " << std::endl;
-  myfile << "   /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/weights_Jet/TMVAnalysis_BDT.weights.xml, \\" << std::endl;
-  myfile << "   /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/weights_Jet/TMVAnalysis_Fisher.weights.xml, \\" << std::endl;
-  myfile << "   /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/weights_Jet/TMVAnalysis_Likelihood.weights.xml, \\" << std::endl;
-  myfile << "   /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/weights_Jet/TMVAnalysis_LikelihoodD.weights.xml" << std::endl;
-  
-  myfile << "Methods = \\" << std::endl;
-  myfile << "   MLP, \\ " << std::endl;
-  myfile << "   BDT, \\ " << std::endl;
-  myfile << "   Fisher, \\" << std::endl;
-  myfile << "   Likelihood, \\" << std::endl;
-  myfile << "   LikelihoodD" << std::endl;
-  myfile << std::endl;
-  
-  
-  myfile << "[Lepton]" << std::endl;
-  myfile << "AdditionalInputFiles = \\" << std::endl;
-  myfile << "   /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/weights_Lep/TMVAnalysis_MLP.weights.xml, \\ " << std::endl;
-  myfile << "   /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/weights_Lep/TMVAnalysis_BDT.weights.xml, \\" << std::endl;
-  myfile << "   /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/weights_Lep/TMVAnalysis_Fisher.weights.xml, \\" << std::endl;
-  myfile << "   /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/weights_Lep/TMVAnalysis_Likelihood.weights.xml, \\" << std::endl;
-  myfile << "   /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/weights_Lep/TMVAnalysis_LikelihoodD.weights.xml" << std::endl;
-  
-  myfile << "Methods = \\" << std::endl;
-  myfile << "   MLP, \\ " << std::endl;
-  myfile << "   BDT, \\" << std::endl;
-  myfile << "   Fisher, \\" << std::endl;
-  myfile << "   Likelihood, \\" << std::endl;
-  myfile << "   LikelihoodD" << std::endl;
+  myfile << "OutFileNameEfficiencies = /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/output/out_NtupleProducer_" << nameSample[iSample] << ".root" << std::endl;
   myfile << std::endl;
   
   myfile.close(); 
--- AnalysisPackage_qqHWWlnulnu/test/Spring10/cfgLauncher_FinalSelection_HERCULES.cxx
+++ AnalysisPackage_qqHWWlnulnu/test/Spring10/cfgLauncher_FinalSelection_HERCULES.cxx
@@ -1,30 +1,28 @@
 {
+
+
  char *nameSample[1000];
- char *nameSamplePrefix[1000];
- char *nameSampleTree[1000];
- 
+ char *nameSampleFile[1000];
  char* xsection[1000];
  
  int numberOfSamples = 0;
- std::ifstream inFile("test/Spring10/samples.txt");
+ std::ifstream inFile("test/Spring10/samples_skimmed.txt");
  std::string buffer;
  
  while(!inFile.eof()){
   getline(inFile,buffer);
   std::cout << "buffer = " << buffer << std::endl;
   if (buffer != ""){ ///---> save from empty line at the end!
-   if (buffer.at(0) != '#'){
+   if (buffer[0] != '#'){
     std::stringstream line( buffer );       
     nameSample[numberOfSamples] = new char [1000];
     line >> nameSample[numberOfSamples]; 
     std::cout << nameSample[numberOfSamples] << " ";
     
-    nameSamplePrefix[numberOfSamples] = new char [1000];
-    line >> nameSamplePrefix[numberOfSamples];
-    
-    nameSampleTree[numberOfSamples] = new char [1000];
-    line >> nameSampleTree[numberOfSamples];
-    
+    nameSampleFile[numberOfSamples] = new char [1000];
+    line >> nameSampleFile[numberOfSamples]; 
+    std::cout << nameSampleFile[numberOfSamples] << " ";
+
     xsection[numberOfSamples] = new char [1000];
     line >> xsection[numberOfSamples]; 
     std::cout << xsection[numberOfSamples] << " ";
@@ -37,7 +35,7 @@
  for (int iSample = 0; iSample < numberOfSamples; iSample++){
   std::ofstream myfile;
   char nameFile[1000];
-  sprintf(nameFile,"dir_cfg/do_FinalSelection_%s.sh",nameSample[iSample]);
+  sprintf(nameFile,"test/Spring10/dir_cfg_skimmed/NtupleProducer_%s.sh",nameSample[iSample]);
   myfile.open (nameFile);
 
   myfile << "#!/bin/sh" << std::endl;
@@ -46,7 +44,7 @@
   myfile << "cmsenv" << std::endl;
   myfile << "cd /gwpool/users/amassiro/VBF/Analysis/AnalysisPackage_qqHWWlnulnu/" << std::endl;
   myfile << "source scripts/setup.sh" << std::endl;
-  myfile << "./bin/FinalSelection_Producer.exe test/Spring10/dir_cfg/FinalSelection_" << nameSample[iSample] << ".cfg" << std::endl;
+  myfile << "./bin/NtupleProducer.exe test/Spring10/dir_cfg_skimmed/NtupleProducer_" << nameSample[iSample] << ".cfg" << std::endl;
   myfile << std::endl;
 
   myfile.close(); 
@@ -55,7 +53,7 @@
   TString Command2Line = Form("chmod +x %s",nameFile);
   gSystem->Exec(Command2Line);
 
-  Command2Line = Form("qsub -V -d ./ -q production %s",nameFile);
+  Command2Line = Form("qsub -V -d ./ -q shortcms %s",nameFile);
   gSystem->Exec(Command2Line);
   
  }
