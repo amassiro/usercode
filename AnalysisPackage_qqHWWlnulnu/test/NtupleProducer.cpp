b10c7180da82bd01d214289d59a47c9c8cef0a66
amassiro <>
2011-05-09 10:20:22 +0000

    new function eleIso PU

--- AnalysisPackage_qqHWWlnulnu/test/NtupleProducer.cpp
+++ AnalysisPackage_qqHWWlnulnu/test/NtupleProducer.cpp
@@ -612,8 +612,8 @@ if (debug) std::cerr << " q1 = " << q1 << " : q2 = " << q2 << std::endl;
   ///----  hardcoded fixed preselections ---- VBF (begin) ----
   if (vars.q1_pT < 20.) continue;
   if (vars.q2_pT < 15.) continue;
-  if (vars.M_qq < 200.) continue;
-  if (vars.DEta_qq < 1.) continue;
+  if (vars.M_qq <   0.) continue;
+  if (vars.DEta_qq < 0.) continue;
   ///----  hardcoded fixed preselections ---- VBF (end) ----
   
   
