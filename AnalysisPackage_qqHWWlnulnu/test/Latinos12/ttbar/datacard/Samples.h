d7a4ae1fd24a6a18cf1f45f27b0fbf9debfa8657
Andrea Massironi <massironi.andrea@gmail.com>
2014-08-26 15:25:20 +0200

    Create Samples.h

---
+++ AnalysisPackage_qqHWWlnulnu/test/Latinos12/ttbar/datacard/Samples.h
@@ -0,0 +1,73 @@
+####################################################################
+#### name_sample            xsection        name_human_readable ####
+####################################################################
+#
+#
+################################################################
+DATA                           -1              DATA
+################################################################
+#
+#
+#
+# HCP # 036_DY10toLLMad                           baseW*effW*triggW*puW*((mctruth==1)*2.14+(mctruth==0)*1.81)    VV-DY-Fake        ZJets-eemm
+# HCP # 037_DY50toLLMad                           baseW*effW*triggW*puW*((mctruth==1)*2.14+(mctruth==0)*1.81)    VV-DY-Fake        ZJets-eemm
+#
+036_DY10toLLMad                           baseW*effW*triggW*puW*((mctruth==1)*2.09+(mctruth==0)*2.01)    VV-DY-Fake        ZJets-eemm
+037_DY50toLLMad                           baseW*effW*triggW*puW*((mctruth==1)*2.09+(mctruth==0)*2.01)    VV-DY-Fake        ZJets-eemm
+#
+DYtt_19.5fb                         effW*triggW*baseW*puW             VV-DY-Fake   ZJets    DYtautau
+#
+# DYtt_12.1fb                               effW*triggW*baseW*puW*mctruth*0.062/19.602*1.112       VV-DY-Fake   ZJets    DYtautau
+#
+## 222_DYtt                                      effW*triggW*baseW/5.063                      VV-DY-Fake
+#
+#   036_DY10toLLMad                           baseW*effW*triggW*puW*(mctruth==2)   VV-DY-Fake       DYtautau
+#   037_DY50toLLMad                           baseW*effW*triggW*puW*(mctruth==2)   VV-DY-Fake      DYtautau
+#
+##
+### 103TT_DoubleMu2012ADYttem                 baseW/5.016399                              VV-DY-Fake
+### 113TT_DoubleMu2012BDYttem                 baseW/5.016399                              VV-DY-Fake
+##
+#
+#
+# 000_WWJets2LMad                     baseW*effW*triggW*puW*(1+(mjj>500)*(detajj>3.5))            VV-DY-Fake            WW
+#
+052_WW2JetsPhantom                  baseW*effW*triggW*puW*(numbLHE==0)*(mjj>100)           VV-DY-Fake       WW2j    WW+2jets
+000_WWJets2LMad                     baseW*effW*triggW*puW                                  VV-DY-Fake            WW
+001_GluGluToWWTo4L                  baseW*effW*triggW*puW                                  VV-DY-Fake            WW
+#
+####         070_WZFull                          0.0022456186*effW*triggW*puW        VV-DY-Fake            WW
+####         077_ZZTo2L2Nu                       baseW*effW*triggW*puW        VV-DY-Fake            VV
+#
+# WJets_fake                                fakeW/12.103              VV-DY-Fake             WJets
+#
+#
+# 080_WJetsToLNuMad               baseW*effW*triggW*puW        VV-DY-Fake            WJets
+#
+LooseLoose_19.5fb                           fakeW/19.5*(run!=201191)              VV-DY-Fake            WJets
+#
+# 178_WJetsToLNuMad                   baseW*effW*triggW*puW        VV-DY-Fake            WJets
+#
+#
+074_WZJetsMad                             baseW*effW*triggW*puW             VV-DY-Fake  
+075_ZZJetsMad                             baseW*effW*triggW*puW             VV-DY-Fake 
+078_WZTo2L2QMad                           baseW*effW*triggW*puW             VV-DY-Fake    
+079_ZZTo2L2QMad                           baseW*effW*triggW*puW             VV-DY-Fake  
+#
+#
+082_WGstarToElNuMad                       1.6*baseW*effW*triggW*puW             VV-DY-Fake  
+083_WGstarToMuNuMad                       1.6*baseW*effW*triggW*puW             VV-DY-Fake
+084_WGstarToTauNuMad                      1.6*baseW*effW*triggW*puW             VV-DY-Fake  
+085_WgammaToLNuG                          baseW*effW*triggW*puW             VV-DY-Fake  
+#
+### 010_TTJetsMad                       baseW*effW*triggW*puW             top
+019_TTTo2L2Nu2B                           baseW*effW*triggW*puW             top
+011_TtWFullDR                             baseW*effW*triggW*puW             top
+012_TbartWFullDR                          baseW*effW*triggW*puW             top
+#
+#
+#
+1130_ggToH130toWWTo2LAndTau2Nu            baseW*effW*triggW*kfW*puW             ggH
+2130_vbfToH130toWWTo2LAndTau2Nu           baseW*effW*triggW*kfW*puW             qqH
+#
+#
